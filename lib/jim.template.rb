0e8fcc5bfc10d4be38822249178e1db1c702cec6
surrim <root@surrim.org>
2024-08-29 21:43:33 +0200

    template added

---
+++ lib/jim.template.rb
@@ -0,0 +1,17 @@
+class Jim
+	attr_reader :template
+
+	DEFAULT_TEMPLATE = "jim-template.html"
+
+	def template(template)
+		@template = template.nil? ? DEFAULT_TEMPLATE : template.to_s
+		self
+	end
+
+	def reset_template() = template(nil)
+
+	module LiquidFilters
+		def jim_template(jim, template) = jim.template(template)
+		def jim_reset_template(jim) = jim.reset_template()
+	end
+end
