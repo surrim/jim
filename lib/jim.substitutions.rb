20e1ddd86e2aee0acf92ac0f8b28eae43dbc501b
surrim <root@surrim.org>
2024-08-29 21:24:05 +0200

    substitutions added

---
+++ lib/jim.substitutions.rb
@@ -0,0 +1,36 @@
+class Jim
+	attr_reader :substitutions
+
+	DEFAULT_SUBSTITUTIONS = {}
+
+	def substitutions(*substitutions)
+		rm_substitutions()
+		{}.merge(*substitutions.compact).each { |key, value|
+			add_substitution(key, value)
+		}
+		self
+	end
+
+	def add_substitution(key, value)
+		if value.nil?
+			@substitutions.delete(key.to_s)
+		else
+			@substitutions[key.to_s] = value.to_s
+		end
+		self
+	end
+
+	def rm_substitution(key) = add_substitution(key, nil)
+	
+	def rm_substitutions()
+		@substitutions = {}
+		self
+	end
+	
+	module LiquidFilters
+		def jim_substitutions(jim, *substitutions) = jim.substitutions(jim, substitutions)
+		def jim_add_substitution(jim, key, value) = jim.add_substitution(key, value)
+		def jim_rm_substitution(jim, key) = jim.rm_substitution(key)
+		def jim_rm_substitutions(jim) = jim.rm_substitutions()
+	end
+end
