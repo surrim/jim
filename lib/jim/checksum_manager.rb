5600e47f28474c183abd4b559679e10916f8f71e
surrim <root@surrim.org>
2024-09-08 15:40:59 +0200

    ChecksumManager added

---
+++ lib/jim/checksum_manager.rb
@@ -0,0 +1,40 @@
+require 'singleton'
+
+class Jim::ChecksumManager
+	include Singleton
+
+	CHECKSUMS_JSON = "checksums.json"
+	JSON_MODE = JSON::PRETTY_STATE_PROTOTYPE.dup
+
+	def initialize
+		@checksums_json = Jim::System.local_cache_path(CHECKSUMS_JSON)
+		@checksums = @checksums_json.exist? ? JSON.parse(@checksums_json.read) : {}
+		ObjectSpace.define_finalizer(self, self.class.method(:finalize))
+	end
+
+	def sha256(filename)
+		unless filename.exist?
+			@checksums.delete(filename.to_s)
+			return nil
+		end
+
+		stat = filename.stat
+		mtime = (10 ** 9) * stat.mtime.tv_sec + stat.mtime.tv_nsec
+		size = stat.size
+
+		checksum_entry = @checksums[filename.to_s] || {}
+		if checksum_entry.has_key?("sha256") && checksum_entry["mtime"] == mtime && checksum_entry["size"] == size
+			return checksum_entry["sha256"]
+		end
+
+		sha256 = Digest::SHA256.file(filename).hexdigest
+		@checksums[filename.to_s] = { "mtime" => mtime, "size" => size, "sha256" => sha256 }
+		sha256
+	end
+
+	def self.sha256(filename) = instance.sha256(filename)
+
+	def finalize = Jim::Utils.write_file(@checksums_json, JSON.generate(@checksums.sort.to_h, JSON_MODE))
+
+	def self.finalize(_object_id) = instance.finalize
+end
--- lib/jim/utils.rb
+++ lib/jim/utils.rb
@@ -10,4 +10,15 @@ module Jim::Utils
 		end
 		result
 	end
+
+	def write_file(filename, content, use_tmp: true)
+		FileUtils.mkdir_p(filename.dirname)
+		if use_tmp
+			tmp_filename = filename.dirname + ".#{filename.basename}.tmp"
+			tmp_filename.write(content)
+			FileUtils.mv(tmp_filename, filename)
+		else
+			filename.write(content)
+		end
+	end
 end
