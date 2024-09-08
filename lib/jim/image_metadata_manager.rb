65171840e173ee285f3016a8acdc83e97ca3c991
surrim <root@surrim.org>
2024-09-08 22:10:21 +0200

    ImageMetadataManager simplified

--- lib/jim/image_metadata_manager.rb
+++ lib/jim/image_metadata_manager.rb
@@ -1,24 +1,17 @@
-require 'singleton'
-
-class Jim::ImageMetadataManager
-	include Singleton
+module Jim::ImageMetadataManager
+	module_function
 
 	IMAGE_METADATA_JSON_PATTERN = "metadata/%{sha256}.json"
 	RATIONALIZE_TOLERANCE = 0.005
 
-	def initialize
-		@image_metadata_cache = {}
-	end
-
-	def all_data(filename)
+	def metadata(filename)
 		sha256 = Jim::ChecksumManager.sha256(filename)
 		return {} if sha256.nil?
-		return @image_metadata_cache[sha256] if @image_metadata_cache[sha256]
 
 		image_metadata_json = Jim::System.cache_path(IMAGE_METADATA_JSON_PATTERN % { sha256: sha256 })
 		if image_metadata_json.exist?
 			FileUtils.touch(image_metadata_json)
-			return @image_metadata_cache[sha256] = JSON.parse(image_metadata_json.read, { symbolize_names: true })
+			return JSON.parse(image_metadata_json.read, { symbolize_names: true })
 		end
 
 		image = Jim::Utils::load_image(filename)
@@ -42,30 +35,4 @@ class Jim::ImageMetadataManager
 		Jim::Utils.write_json_file(image_metadata_json, image_metadata)
 		image_metadata
 	end
-
-	def width(filename) = all_data(filename)[:width]
-
-	def height(filename) = all_data(filename)[:height]
-
-	def simplified_width(filename) = all_data(filename)[:simplified_width]
-
-	def simplified_height(filename) = all_data(filename)[:simplified_height]
-
-	def mime_type(filename) = all_data(filename)[:mime_type]
-
-	def avg_color(filename) = all_data(filename)[:avg_color]
-
-	def self.all_data(filename) = instance.all_data(filename)
-
-	def self.width(filename) = instance.width(filename)
-
-	def self.height(filename) = instance.height(filename)
-
-	def self.simplified_width(filename) = instance.simplified_width(filename)
-
-	def self.simplified_height(filename) = instance.simplified_height(filename)
-
-	def self.mime_type(filename) = instance.mime_type(filename)
-
-	def self.avg_color(filename) = instance.avg_color(filename)
 end
