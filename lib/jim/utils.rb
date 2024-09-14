1573b306026ed751aa658a6d0883bab74f5bbd4c
surrim <root@surrim.org>
2024-09-14 15:12:43 +0200

    tmp_filename() and prepare_folder()

--- lib/jim/utils.rb
+++ lib/jim/utils.rb
@@ -19,15 +19,19 @@ module Jim::Utils
 	end
 
 	def write_file(filename, content, use_tmp: true)
-		FileUtils.mkdir_p(filename.dirname)
+		prepare_folder(filename)
 		if use_tmp
-			tmp_filename = filename.dirname + ".#{filename.basename}.tmp"
+			tmp_filename = tmp_filename(filename)
 			tmp_filename.write(content)
 			FileUtils.mv(tmp_filename, filename)
 		else
 			filename.write(content)
 		end
 	end
+
+	def tmp_filename(filename) = filename.dirname + ".#{filename.basename}.tmp"
+
+	def prepare_folder(filename) = FileUtils.mkdir_p(filename.dirname)
 
 	def write_json_file(filename, content, use_tmp: true) = write_file(filename, JSON.generate(content, JSON_MODE), use_tmp: use_tmp)
 
