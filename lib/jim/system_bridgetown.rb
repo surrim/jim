3b8f6f2220cc58202faec5b5e0c1df421f1db62e
surrim <root@surrim.org>
2025-02-09 21:47:55 +0100

    fixed Style/EvalWithLocation warnings

--- lib/jim/system_bridgetown.rb
+++ lib/jim/system_bridgetown.rb
@@ -14,8 +14,8 @@ if Module.const_defined?(:Bridgetown)
         relative_destination_filename.dirname.to_s,
         relative_destination_filename.basename.to_s
       )
-      static_file.instance_eval "def path = #{source_filename.to_s.dump}"
-      static_file.instance_eval 'def modified_time = Time.at(0)'
+      static_file.instance_eval("def path = #{source_filename.to_s.dump}", __FILE__, __LINE__)
+      static_file.instance_eval('def modified_time = Time.at(0)', __FILE__, __LINE__)
       static_file.write('')
       @site.static_files.push(static_file)
     end
