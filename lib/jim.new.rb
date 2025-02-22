94aeb056c95b1beeea8538d59c969645e44e9db4
surrim <root@surrim.org>
2025-02-22 17:39:42 +0100

    better Jim.new() code

--- lib/jim.new.rb
+++ lib/jim.new.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Jim
-  def initialize(src, alt = nil, **presets)
+  def initialize(src, alt = nil, *presets, **preset_options)
     @src = src.to_s
     @alt = alt&.to_s
 
@@ -12,12 +12,18 @@ class Jim
       default_value = self.class.const_get(constant)
       method(method_name).call(default_value)
     end
-    presets.each do |method_name, value|
+
+    merged_presets = {}.merge(*presets.compact
+           .map { |preset| preset.is_a?(Hash) ? preset : preset.to_h }
+           .map { |preset| preset.transform_keys(&:to_sym) }, preset_options)
+    merged_presets.each do |method_name, value|
       method(method_name).call(value)
+    rescue NameError
+      Jim::System.info("Ignored preset parameter #{method_name} = #{value.inspect}")
     end
   end
 
   module LiquidFilters
-    def jim_new(src, alt = nil, *presets) = Jim.new(src, alt, **{}.merge(*presets.compact))
+    def jim_new(src, alt = nil, *presets) = Jim.new(src, alt, *presets)
   end
 end
